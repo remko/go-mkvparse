210a9a0a2324bc1da1f6060cc82048ff31ee4382
Remko Tronçon <remko@el-tramo.be>
2022-01-16 10:45:46 +0100

    handlers: remove ParseCoverImage again to avoid dependency

--- handlers/cover_test.go
+++ handlers/cover_test.go
@@ -1,7 +1,12 @@
 package mkvparse
 
 import (
+	"bytes"
 	"fmt"
+	"image"
+	"image/jpeg"
+	"image/png"
+	"log"
 )
 
 func ExampleParseCover() {
@@ -13,4 +18,31 @@ func ExampleParseCover() {
 
 	// Output:
 	// parsed cover: image/jpeg (41363 bytes)
+}
+
+func ExampleParseCover_Image() {
+	data, typ, err := ParseCover("../example-cover.mkv")
+	if err != nil {
+		log.Panic(err)
+	}
+	if data == nil {
+		log.Panic("no cover")
+		return
+	}
+	var img image.Image
+	switch typ {
+	case "image/jpeg":
+		img, err = jpeg.Decode(bytes.NewReader(data))
+	case "image/png":
+		img, err = png.Decode(bytes.NewReader(data))
+	default:
+		log.Panicf("unknown MIME type: %s", typ)
+	}
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Printf("parsed cover image: %dx%d\n", img.Bounds().Dx(), img.Bounds().Dy())
+
+	// Output:
+	// parsed cover image: 265x377
 }
